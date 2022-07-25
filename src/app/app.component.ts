b2498f4c07a04f3345b5d22005b525586aa2827a
Alexandra2888 <moldovan.alexandra28@gmail.com>
2022-07-25 13:49:28 +0300

    finishing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Pipes';
+
+  name = ''
+  date = ''
+  amount =''
+
+  onNameChange(event: Event)  {
+this.name = (<HTMLInputElement>event.target).value
+  }
+
+  onDateChange(event: Event) {
+    this.date = (<HTMLInputElement>event.target).value
+  }
+
+  onAmountChange(event: Event) {
+    this.amount = (<HTMLInputElement>event.target).value
+  }
 }
